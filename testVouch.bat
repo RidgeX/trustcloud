ae36a0cab86a78d6418dcde6285bdc85a3559080
RidgeX <ridge.shrubsall@gmail.com>
2014-05-22 13:39:32 +0800

    Allow test scripts to take host parameter

--- testVouch.bat
+++ testVouch.bat
@@ -1,28 +1,28 @@
 @echo off
-call client -a res/cloud.jpg
-call client -u users/A.crt
-call client -u users/B.crt
-call client -u users/C.crt
-call client -u users/D.crt
-call client -u users/E.crt
-call client -u users/F.crt
-call client -u users/G.crt
-call client -u users/H.crt
-call client -v res/cloud.jpg users/F.crt
-call client -v users/D.crt users/H.crt
-call client -v users/H.crt users/D.crt
-call client -v users/G.crt users/H.crt
-call client -v users/D.crt users/C.crt
-call client -v users/C.crt users/D.crt
-call client -v users/G.crt users/C.crt
-call client -v users/G.crt users/F.crt
-call client -v users/F.crt users/G.crt
-call client -v users/C.crt users/B.crt
-call client -v users/F.crt users/B.crt
-call client -v users/F.crt users/E.crt
-call client -v users/E.crt users/B.crt
-call client -v users/B.crt users/A.crt
-call client -v users/A.crt users/E.crt
-call client -a res/hello.txt
-call client -u users/I.crt
-call client -v res/hello.txt users/I.crt
+call client -a res/cloud.jpg %*
+call client -u users/A.crt %*
+call client -u users/B.crt %*
+call client -u users/C.crt %*
+call client -u users/D.crt %*
+call client -u users/E.crt %*
+call client -u users/F.crt %*
+call client -u users/G.crt %*
+call client -u users/H.crt %*
+call client -v res/cloud.jpg users/F.crt %*
+call client -v users/D.crt users/H.crt %*
+call client -v users/H.crt users/D.crt %*
+call client -v users/G.crt users/H.crt %*
+call client -v users/D.crt users/C.crt %*
+call client -v users/C.crt users/D.crt %*
+call client -v users/G.crt users/C.crt %*
+call client -v users/G.crt users/F.crt %*
+call client -v users/F.crt users/G.crt %*
+call client -v users/C.crt users/B.crt %*
+call client -v users/F.crt users/B.crt %*
+call client -v users/F.crt users/E.crt %*
+call client -v users/E.crt users/B.crt %*
+call client -v users/B.crt users/A.crt %*
+call client -v users/A.crt users/E.crt %*
+call client -a res/hello.txt %*
+call client -u users/I.crt %*
+call client -v res/hello.txt users/I.crt %*
